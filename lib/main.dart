99791f483427fff5e7b75f3a1330aeff24089f04
Júlio Nery <julio_cesar.an@hotmail.com>
2025-04-11 23:15:59 -0300

    feat(core): add localization support for English and Portuguese

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dev_test/generated/l10n.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dev Flutter Test',
+      title: S.of(context).appName,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
